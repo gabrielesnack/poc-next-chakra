802eefc2d18ce0169283ef1ccfa753638736842e
Gabriel Esnack <gabriel_esnack@hotmail.com>
2020-11-28 21:09:12 -0300

    update added chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/app'
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/react';
 import '@/styles/globals.css'
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
