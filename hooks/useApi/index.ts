0528143450674586bd272798fe24e0c395dea7ac
Gabriel Esnack <gabriel_esnack@hotmail.com>
2020-12-05 01:35:18 -0300

    fixed useApi

--- hooks/useApi/index.ts
+++ hooks/useApi/index.ts
@@ -4,11 +4,10 @@ import { IResponse } from './types'
 const useApi = (url, payload = {}) => {
   const [response, setResponse] = useState<IResponse>({
     error: false,
-    status: 'idle',
+    status: 'pending',
   })
 
   useEffect(() => {
-    setResponse((state) => ({ ...state, status: 'pending' }))
     fetch(url, payload)
       .then(async (res) => {
         const resJson = await res.json()
