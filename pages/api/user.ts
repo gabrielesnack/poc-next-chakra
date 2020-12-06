07d2875b281a2619b27369a0e8729aff1057ab80
Gabriel Esnack <gabriel_esnack@hotmail.com>
2020-12-06 03:50:37 -0300

    added get user

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -4,8 +4,20 @@ import users from '@/mock/users'
 export default (req, res) => {
   if (req.method == 'GET') {
     const cookies = cookie.parse(req.headers.cookie || '')
-    console.log(cookies.token)
-    res.statusCode = 200
-    res.json()
+    const user = findUser(cookies)
+    if (user) {
+      res.statusCode = 200
+      res.json(user)
+    } else {
+      res.statusCode = 404
+      res.json({
+        error: true,
+        message: 'User not Found',
+      })
+    }
   }
+}
+
+const findUser = ({ token }) => {
+  return users.find((e) => e.token == token)
 }
